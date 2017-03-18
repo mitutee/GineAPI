33faaf0cccc70e2a7b28c7e8606e8ac99ae64dac
mitutee <ex@ex.ex>
2017-03-18 12:36:27 +0200

    Initial for new vk-dotnet proj

---
+++ vk-dotnet/vk-dotnet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace vk_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Session s = new Session();
+            s.users.get("210700286");
+            Thread.Sleep(9999);
+        }
+    }
+}
---
+++ vk-dotnet/vk-dotnet/Session.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+
+namespace vk_dotnet
+{
+    class Session
+    {
+        private HttpClient client = new HttpClient();
+
+        public Session(){}
+
+
+        public Users users = Users.TheInstance;
+    }
+}
