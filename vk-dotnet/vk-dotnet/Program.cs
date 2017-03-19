b46166f8cd24230898d6a6d8f1e31dc1d9dbf6a5
mitutee <ex@ex.ex>
2017-03-19 08:13:20 +0200

    lg_h extracted

--- vk-dotnet/vk-dotnet/Program.cs
+++ vk-dotnet/vk-dotnet/Program.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Text;
 using vk_dotnet.Objects;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace vk_dotnet
 {
@@ -15,26 +16,50 @@ namespace vk_dotnet
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Session s = new Session(t);
 
-            while (true)
-            {
-                string m = Console.ReadLine();
-                s.Messages.Send("80314023", m);
-            }
-            
+            Auth("+380505102671", "oJolgolo243VK");
 
             Console.ReadKey();
         }
-
-        static async Task AsyncMothefucker(Session s)
+        //\"lg_h\" value=\"([a - z0 - 9] +)\"
+        public static async Task<string> Auth(string login, string password)
         {
-            List<User> result = await s.Users.Get("94394992", "1" , "123456");
+            string REGEX_LOGIN_HASH = @"lg_h=(\d+\w+)";
 
-            result.ForEach(user => Console.WriteLine(user.first_name));
-            
+            using (var cl = new HttpClient())
+            {
+                //cl.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+                //cl.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate");
+                //cl.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36");
+                //cl.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Charset", "ISO-8859-1");
+                
+                Console.WriteLine("Headers have been set.");
+                HttpResponseMessage response = await cl.GetAsync("http://vk.com/");
+                string response_text = await response.Content.ReadAsStringAsync();
 
-        } 
+                Console.WriteLine(response_text);
+
+                string lg_h = extractWithGivenPattern(REGEX_LOGIN_HASH, response_text);
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("act", "login"),
+                    new KeyValuePair<string, string>("al_frame", "al_frame"),
+                    new KeyValuePair<string, string>("_origin", "https://vk.com"),
+                    new KeyValuePair<string, string>("utf8", "1"),
+                    new KeyValuePair<string, string>("email", login),
+                    new KeyValuePair<string, string>("pass", password),
+                    new KeyValuePair<string, string>("lg_h", lg_h),
+                });
+                return "";
+            }
+        }
+
+        private static string extractWithGivenPattern(string pattern, string text)
+        {
+            Regex p = new Regex(pattern);
+            Match match = p.Match(text);
+            return match.Groups[1].Value;
+        }
 
 
     }
